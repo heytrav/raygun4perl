37d85a0f249f3db7950852589833b59609c10f46
Travis Holton <travis@catalyst.net.nz>
2014-06-26 11:38:38 +1200

    adding initial perl modules

---
+++ lib/Raygun4perl.pm
@@ -0,0 +1,38 @@
+package Raygun4perl;
+
+use Mouse;
+
+=head1 NAME
+
+Raygun4perl - Connect to the Raygun.io API
+
+=head1 SYNOPSIS
+
+  use Raygun4perl;
+
+  # synopsis...
+
+=head1 DESCRIPTION
+
+# longer description...
+
+
+=head1 INTERFACE
+
+=cut
+
+
+
+
+
+=head1 DEPENDENCIES
+
+
+=head1 SEE ALSO
+
+=cut
+
+
+1;
+
+__END__
---
+++ lib/Raygun4perl/Messenger.pm
@@ -0,0 +1,88 @@
+package Raygun4perl::Messenger;
+
+use Mouse;
+
+use LWP::UserAgent;
+use URI;
+
+
+=head1 NAME
+
+Raygun4perl::Messenger - Communicate with the Raygun.io endpoint.
+
+=head1 SYNOPSIS
+
+  use Raygun4perl::Messenger;
+
+  my $raygun = Raygun4perl::Messenger->new(api_key => 'your key here');
+  my $response = $raygun->post($exception);
+  # $response->status == ?
+
+=head1 DESCRIPTION
+
+# longer description...
+#
+
+
+
+=head1 INTERFACE
+
+=cut
+
+has api_key => (
+    is	    => 'rw',
+    isa 	=> 'Str',
+    required => 1
+);
+
+
+
+has api_endpoint => (
+    is	    => 'ro',
+    isa 	=> 'URI',
+    default => sub {
+        return URI->new('https://api.raygun.io/entries');
+    },
+);
+
+
+has user_agent => (
+    is	    => 'ro',
+    isa 	=> 'LWP::UserAgent',
+    default => sub {
+        return LWP::UserAgent->new();
+    },
+);
+
+
+=head2 post_to_raygun
+
+Send data to api.raygun.io/entries via a POST request.
+
+=cut
+
+sub post_to_raygun {
+    my ($self, $args) = @_;
+    my $uri = $self->api_endpoint;
+    my $ua = $self->user_agent;
+    my $response = $ua->post($uri, );
+    return $response;
+
+}
+
+
+
+
+
+
+=head1 DEPENDENCIES
+
+
+=head1 SEE ALSO
+
+
+=cut
+
+1;
+
+__END__
