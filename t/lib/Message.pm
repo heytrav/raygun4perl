7a6a46ffd01720a23a0d50f3d610ecca9ff33e3f
Travis Holton <travis@catalyst.net.nz>
2014-06-27 10:47:55 +1200

    setup tests for raygun message

---
+++ t/lib/Message.pm
@@ -0,0 +1,35 @@
+package Message;
+
+use strict;
+use warnings;
+
+use parent qw(Test::Class);
+
+use Test::More;
+use Test::Deep;    # (); # uncomment to stop prototype errors
+use Test::Exception;
+
+use Smart::Comments;
+
+sub prep001_message_available : Test(startup => 1) {
+    my $self = shift;
+    use_ok('Raygun4perl::Message')
+      or $self->FAIL_ALL("Message class not available.");
+}
+
+sub t0010_form_raygun_message : Test(3) {
+    my $self = shift;
+    my $message;
+    lives_ok {
+        $message =
+          Raygun4perl::Message->new( occurred_on => '2014-06-27T03:15:10' );
+    }
+    'Instantiated Message object.';
+    my $occurred_on = $message->occurred_on;
+    isa_ok( $occurred_on, 'DateTime', 'Correct object' );
+
+}
+
+1;
+
+__END__
--- t/lib/Messenger.pm
+++ t/lib/Messenger.pm
@@ -11,17 +11,16 @@ use Test::Exception;
 
 use Smart::Comments;
 
-
 sub prep001_messenger_available : Test(startup => 1) {
     my $self = shift;
-    use_ok('Raygun4perl::Messenger') 
-        or $self->FAIL_ALL('Messenger class not available.');
+    use_ok('Raygun4perl::Messenger')
+      or $self->FAIL_ALL('Messenger class not available.');
 
 }
 
-sub t0010_raygun_http_403_response :Test(3) {
+sub t0010_raygun_http_403_response : Test(3) {
     my $self = shift;
-    my $messenger = Raygun4perl::Messenger->new(api_key => '');
+    my $messenger = Raygun4perl::Messenger->new( api_key => '' );
     my $result;
     lives_ok {
         $result = $messenger->post_to_raygun( {} );
@@ -31,9 +30,8 @@ sub t0010_raygun_http_403_response :Test(3) {
     cmp_ok( $result->code, '==', 403, 'Expect a "Bad Request" error.' );
 }
 
-
 sub t0020_raygun_http_400_response : Test(2) {
-    my $self = shift;
+    my $self    = shift;
     my $api_key = $ENV{RAYGUN_API_KEY};
     if ( not defined $api_key ) {
         $self->SKIP_ALL('No API key for Raygun.io. No point in continuing.');
@@ -50,7 +48,6 @@ sub t0020_raygun_http_400_response : Test(2) {
     cmp_ok( $result->code, '==', 400, 'Expect a "Bad Request" error.' );
 
 }
-
 
 1;
 
