7765c5e09ab513bc390d476770ee3666d91f06aa
Travis Holton <travis@catalyst.net.nz>
2014-06-27 15:47:43 +1200

    whitespace

--- t/lib/Message.pm
+++ t/lib/Message.pm
@@ -40,7 +40,7 @@ sub t0010_validate_raygun_occurred_on : Test(3) {
 
 sub t0020_validate_line_number : Test(4) {
     my $self = shift;
-    my $message ;
+    my $message;
     lives_ok {
         $message = Raygun4perl::Message->new(
             error => {
@@ -50,9 +50,12 @@ sub t0020_validate_line_number : Test(4) {
     }
     'Instantiated Message object.';
     my $error = $message->error;
-    isa_ok($error, 'Raygun4perl::Message::Error', 'Error is an error type');
-    isa_ok($error->stack_trace->[0], 'Raygun4perl::Message::Error::StackTrace', 
-    'Stack trace is the correct type.');
+    isa_ok( $error, 'Raygun4perl::Message::Error', 'Error is an error type' );
+    isa_ok(
+        $error->stack_trace->[0],
+        'Raygun4perl::Message::Error::StackTrace',
+        'Stack trace is the correct type.'
+    );
     throws_ok {
         Raygun4perl::Message->new(
             error => {
