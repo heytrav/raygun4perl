4a22eb9c86b71bd1c97c88955be24a4fce820fc3
Harald Jörg <Harald.Joerg@arcor.de>
2018-09-05 13:50:55 +0200

    Move COPYRIGHT AND LICENSE to the main module

--- lib/WebService/Raygun/Message.pm
+++ lib/WebService/Raygun/Message.pm
@@ -373,13 +373,6 @@ sub prepare_raygun {
 =back
 
 
-=head1 COPYRIGHT AND LICENSE
-
-This software is copyright (c) 2014 by Travis Holton.
-
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
-
 =cut
 
 __PACKAGE__->meta->make_immutable();
